1e9c21ead70565b21a5a2e59b1cf891c3679dbf4
slc <slc>
2017-09-15 19:21:54 +0530

    stylings

--- app.js
+++ app.js
@@ -6,6 +6,26 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+app.get('/assets/bootstrap.min.css', function (req, res) {
+  res.sendFile(path.join(__dirname + '/assets/bootstrap.min.css'));
+});
+
+app.get('/assets/jquery.min.js', function (req, res) {
+  res.sendFile(path.join(__dirname + '/assets/jquery.min.js'));
+});
+
+app.get('/assets/jspdf.min.js', function (req, res) {
+  res.sendFile(path.join(__dirname + '/assets/jspdf.min.js'));
+});
+
+app.get('/assets/images/user.png', function (req, res) {
+  res.sendFile(path.join(__dirname + '/assets/images/user.png'));
+});
+
+app.get('/assets/images/assistant.png', function (req, res) {
+  res.sendFile(path.join(__dirname + '/assets/images/assistant.png'));
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 });
